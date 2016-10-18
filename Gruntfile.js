c4ef8f2c697773c74c37fd2f485b74bc9c860ebe
Fotis Stamatelopoulos <fotisstamatelopoulos@upwork.com>
2016-10-18 12:49:05 -0700

    changed license to Apache version 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,8 +99,14 @@ module.exports = function(grunt) {
                             '*\n' +
                             '* Copyright (C) 2016 Upwork.com\n' +
                             '*\n' +
-                            '* This software may be modified and distributed under the terms\n' +
-                            '* of the MIT license.  See the LICENSE file for details.\n' +
+                            '* This software licensed under the Apache license, Version 2.0 (the "License")\n' +
+                            '* you may not use this file except in compliance with the License.\n' +
+                            '* You may obtain a copy of the License at\n*\nhttp://www.apache.org/licenses/LICENSE-2.0\n*\n' +
+                            '* Unless required by applicable law or agreed to in writing, software\n' +
+                            'distributed under the License is distributed on an "AS IS" BASIS,\n' +
+                            '* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n' +
+                            '* See the License for the specific language governing permissions and\n' +
+                            '* limitations under the License.' +
                             '**/\n' +
                             '\n';
                         return license +
